24e3e09240785ed108865f4f74e18d8edde803e2
yolo <yolowex@gmail.com>
2022-12-31 09:35:21 +0330

    Added get_percent to Functions.py

--- src/Structures/Functions.py
+++ src/Structures/Functions.py
@@ -1,7 +1,12 @@
 import pygame as pg
+
 def safe_image_load(path:str) -> pg.surface.Surface or None:
     try:
         return pg.image.load(path)
-    except Exception:
+    except OSError:
         return None
 
+def get_percent(All:float,part:float):
+    return part * ( 100 / All )
+
+print(get_percent(1000,320))
