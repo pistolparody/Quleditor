5e2ebf734387bdbd7991394574ae70069428cadd
yolo <yolowex@gmail.com>
2022-12-31 22:06:10 +0330

    Filled Pos.py

--- src/mygame/structures/Pos.py
+++ src/mygame/structures/Pos.py
@@ -1,16 +1,130 @@
-import pygame as pg
+from pygame.math import Vector2
 
-class Pos(pg.math.Vector2):
-    def __init__( self,x:float=0,y:float=0,pos=None ) -> None :
-        if pos is not None:
+
+class Pos(Vector2) :
+
+    def __init__( self, x: float = 0, y: float = 0, pos=None ) -> None :
+        if pos is not None :
             super().__init__(pos)
             return
 
-        super().__init__(x,y)
+        super().__init__(x, y)
 
-    def reset( self,new_x:float=0,new_y:float=0,pos=None, ):
-        if pos is not None:
-            self.x,self.y = pos.x,pos.y
+
+    def copy( self ) :
+        return Pos(pos=self)
+
+
+    def reset( self, new_x: float = 0, new_y: float = 0, pos=None, ) :
+        if pos is not None :
+            self.x, self.y = pos.x, pos.y
             return
 
-        self.x,self.y = new_x,new_y
+        self.x, self.y = new_x, new_y
+
+
+    def lerp_me( self, pos, value: float ) :
+        lerped = self.lerp(pos, value)
+        self.x, self.y = lerped.x, lerped.y
+
+
+    def join( self, pos ) :
+        copy_cat = self.copy()
+        copy_cat.x += pos.x
+        copy_cat.y += pos.y
+
+        return self
+
+
+    def join_me( self, pos ) :
+        self.x += pos.x
+        self.y += pos.y
+
+        return self
+
+
+    def sum( self, sum_x: float = 0, sum_y: float = 0 ) :
+        copy_cat = self.copy()
+        copy_cat.x += sum_x
+        copy_cat.y += sum_y
+
+        return copy_cat
+
+
+    def sum_me( self, sum_x: float = 0, sum_y: float = 0 ) :
+        self.x += sum_x
+        self.y += sum_y
+
+        return self
+
+
+    def mult( self, x_mult: float = 1, y_mult: float = 1, pos=None ) :
+        copy_cat = self.copy()
+        if pos is not None :
+            copy_cat.x *= pos.x
+            copy_cat.y *= pos.y
+            return copy_cat
+
+        copy_cat.x, copy_cat.y = copy_cat.x * x_mult, copy_cat.y * y_mult
+        return copy_cat
+
+
+    def mult_me( self, x_mult: float = 1, y_mult: float = 1, pos=None ) :
+        if pos is not None :
+            self.x *= pos.x
+            self.y *= pos.y
+            return self
+
+        self.x, self.y = self.x * x_mult, self.y * y_mult
+        return self
+
+
+    def transform_me( self, sum_xy: float = 0, sum_y: float = None, mult_xy: float = 1,
+            mult_y: float = None ) :
+
+        if sum_y is None:
+            self.sum_me(sum_xy,sum_xy)
+        else:
+            self.sum_me(sum_xy,sum_y)
+
+        if mult_y is None:
+            self.mult_me(mult_xy,mult_xy)
+        else:
+            self.mult_me(mult_xy,mult_y)
+
+        return self
+
+    def transform( self, sum_xy: float = 0, sum_y: float = None, mult_xy: float = 1,
+            mult_y: float = None ) :
+
+        copy_cat = self.copy()
+
+        if sum_y is None :
+            self.sum_me(sum_xy, sum_xy)
+        else :
+            self.sum_me(sum_xy, sum_y)
+
+        if mult_y is None :
+            self.mult_me(mult_xy, mult_xy)
+        else :
+            self.mult_me(mult_xy, mult_y)
+
+        return self
+
+
+    def flip( self, center=None ) :
+        if center is None : center = Pos(0, 0)
+
+        copy_cat = self.copy()
+        copy_cat.x = center.x * 2 - copy_cat.x
+        copy_cat.y = center.y * 2 - copy_cat.y
+
+        return copy_cat
+
+
+    def flip_me( self, center=None ) :
+        if center is None : center = Pos(0, 0)
+        self.x = center.x * 2 - self.x
+        self.y = center.y * 2 - self.y
+
+        return self
